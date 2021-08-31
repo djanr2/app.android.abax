e02493d3fe2250fdf1c2b91b9ca240119ac6a508
djanr2 <djanr2@hotmail.com>
2021-08-30 23:09:18 -0500

    20210830 drag bid

--- app/src/main/java/com/djanr/abax/MainActivity.java
+++ app/src/main/java/com/djanr/abax/MainActivity.java
@@ -1,26 +1,22 @@
 package com.djanr.abax;
 
-import android.content.ClipData;
-import android.content.ClipDescription;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.DragEvent;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.FrameLayout;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+
 
 import com.djanr.abax.utils.Utils;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView image_five_bid;
-    private final String msg = "<j>";
-    private android.widget.FrameLayout.LayoutParams layoutParams;
+    private ImageView main_bid_five;
+
+    private int ypos_bid_five;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,79 +24,31 @@ public class MainActivity extends AppCompatActivity {
         Utils.fullScreen(this);
         setContentView(R.layout.activity_main);
 
-        image_five_bid = findViewById(R.id.main_five_bid);
-
-        image_five_bid.setTag("TAG_J");
-
-        image_five_bid.setOnLongClickListener(new View.OnLongClickListener() {
+        main_bid_five=findViewById(R.id.main_bid_five);
+        main_bid_five.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public boolean onLongClick(View v) {
-                ClipData.Item item = new ClipData.Item((CharSequence)v.getTag());
-                String[] mimeTypes = {ClipDescription.MIMETYPE_TEXT_PLAIN};
+            public boolean onTouch(View v, MotionEvent event) {
+                final int x = (int) event.getRawX();
+                final int y = (int) event.getRawY();
 
-                ClipData dragData = new ClipData(v.getTag().toString(),mimeTypes, item);
-                
-                View.DragShadowBuilder myShadow = new View.DragShadowBuilder(image_five_bid);
-
-                v.startDrag(dragData,myShadow,null,0);
-                return true;
-            }
-        });
-
-        image_five_bid.setOnDragListener(new View.OnDragListener() {
-            @Override
-            public boolean onDrag(View v, DragEvent event) {
-                switch(event.getAction()) {
-                    case DragEvent.ACTION_DRAG_STARTED:
-                        layoutParams = (FrameLayout.LayoutParams)v.getLayoutParams();
-                        Log.i(msg, "Action is DragEvent.ACTION_DRAG_STARTED");
-
-                        // Do nothing
+                switch (event.getAction() & MotionEvent.ACTION_MASK){
+                    case MotionEvent.ACTION_DOWN:
+                        RelativeLayout.LayoutParams  lay = (RelativeLayout.LayoutParams) v.getLayoutParams();
+                        //_xDelta = x - lay.leftMargin;
+                        ypos_bid_five = y - lay.topMargin;
                         break;
-
-                    case DragEvent.ACTION_DRAG_ENTERED:
-                        Log.i(msg, "Action is DragEvent.ACTION_DRAG_ENTERED");
-                        int x_cord = (int) event.getX();
-                        int y_cord = (int) event.getY();
+                    case MotionEvent.ACTION_MOVE:
+                        RelativeLayout.LayoutParams  lay2 = (RelativeLayout.LayoutParams) v.getLayoutParams();
+                        //lay2.leftMargin = x - _xDelta;
+                        lay2.topMargin = y - ypos_bid_five;
+                        lay2.rightMargin = -250;
+                        lay2.bottomMargin = -250;
+                        v.setLayoutParams(lay2);
                         break;
-
-                    case DragEvent.ACTION_DRAG_EXITED :
-                        Log.i(msg, "Action is DragEvent.ACTION_DRAG_EXITED");
-                        x_cord = (int) event.getX();
-                        y_cord = (int) event.getY();
-                        layoutParams.leftMargin = x_cord;
-                        layoutParams.topMargin = y_cord;
-                        v.setLayoutParams(layoutParams);
-                        break;
-
-                    case DragEvent.ACTION_DRAG_LOCATION  :
-                        Log.i(msg, "Action is DragEvent.ACTION_DRAG_LOCATION");
-                        x_cord = (int) event.getX();
-                        y_cord = (int) event.getY();
-                        break;
-
-                    case DragEvent.ACTION_DRAG_ENDED   :
-                        Log.i(msg, "Action is DragEvent.ACTION_DRAG_ENDED");
-
-                        // Do nothing
-                        break;
-
-                    case DragEvent.ACTION_DROP:
-                        Log.i(msg, "ACTION_DROP event");
-
-                        // Do nothing
-                        break;
-                    default: break;
                 }
                 return true;
             }
         });
 
-
-
-
-
-
     }
-
 }
--- app/src/main/java/com/djanr/abax/utils/Utils.java
+++ app/src/main/java/com/djanr/abax/utils/Utils.java
@@ -6,7 +6,7 @@ import android.view.WindowManager;
 
 public class Utils {
     public static void fullScreen(Activity a){
-        a. requestWindowFeature(Window.FEATURE_NO_TITLE);
+        a.requestWindowFeature(Window.FEATURE_NO_TITLE);
         a.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
